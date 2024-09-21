42cccca683db6d56b964de80f1d7d839aae87cbf
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-21 14:42:55 +0530

    User controller class implemented.

---
+++ src/main/java/com/BRAINWAVE_JD_01/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.BRAINWAVE_JD_01.controller;
+
+import com.BRAINWAVE_JD_01.DTO.UserDTO;
+import com.BRAINWAVE_JD_01.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create")
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        UserDTO createdUser = userService.createUser(userDTO);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> loginUser(@RequestBody UserDTO loginDTO) {
+        UserDTO user = userService.login(loginDTO.getName(), loginDTO.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+}
