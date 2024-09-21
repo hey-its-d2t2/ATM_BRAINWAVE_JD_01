9c853ec256203a9f5edc0fa2b449ff7732f17fed
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-21 11:34:11 +0530

    First Commit: project created from start.spring.io with these config spring 3.3.4, java 17,JAR, MAVEN, Dep: Web, Lombok, MySQL, JPA, Devtool.

---
+++ src/main/java/com/BRAINWAVE_JD_01/BrainwaveJd01Application.java
@@ -0,0 +1,13 @@
+package com.BRAINWAVE_JD_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainwaveJd01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainwaveJd01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/BRAINWAVE_JD_01/BrainwaveJd01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BRAINWAVE_JD_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainwaveJd01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
